b4066eb505a4af2f1679a0072077c8d93b0d5572
Mohammed <m97.chahboun@gmail.com>
2025-01-11 00:37:55 +0100

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
       title: 'Flexible Wrap Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flexible Wrap Demo'),
     );
@@ -31,7 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final padding = 8.0;
+  final spacing = 12.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,45 +39,41 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: SingleChildScrollView(
-        child: ColoredBox(
-          color: Colors.brown,
-          child: FlexibleWrap(
-            spacing: 12.0,
-            runSpacing: 12.0,
-            textDirection: TextDirection.rtl,
-            children: List.generate(20, (int index) {
-              return Container(
-                height: 100,
-                width: 300,
-                decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(8.0)),
-                child: const Center(
-                  child: ListTile(
-                    title: Text(
-                      "Lorem Ipsum is simply dummy text",
-                      style: TextStyle(color: Colors.white),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    subtitle: Text(
-                      "Lorem Ipsum has been the industry's standard",
-                      style: TextStyle(color: Colors.white),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    leading: Icon(
-                      Icons.insert_emoticon,
-                      color: Colors.white,
-                      size: 60.0,
-                    ),
-                    trailing: Icon(
-                      Icons.favorite,
-                      color: Colors.white,
-                    ),
+        child: FlexibleWrap(
+          spacing: spacing,
+          runSpacing: spacing,
+          textDirection: TextDirection.rtl,
+          children: List.generate(20, (int index) {
+            return Container(
+              height: 100,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(8.0)),
+              child: const Center(
+                child: ListTile(
+                  title: Text(
+                    "Lorem Ipsum is simply dummy text",
+                    style: TextStyle(color: Colors.white),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  subtitle: Text(
+                    "Lorem Ipsum has been the industry's standard",
+                    style: TextStyle(color: Colors.white),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  leading: Icon(
+                    Icons.insert_emoticon,
+                    color: Colors.white,
+                    size: 60.0,
+                  ),
+                  trailing: Icon(
+                    Icons.favorite,
+                    color: Colors.white,
                   ),
                 ),
-              );
-            }).toList(),
-          ),
+              ),
+            );
+          }).toList(),
         ),
       ),
     );
