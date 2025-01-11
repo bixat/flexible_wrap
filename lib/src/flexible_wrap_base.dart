cbec6d52808f23c6aca665c234dc03663f140402
Mohammed <m97.chahboun@gmail.com>
2025-01-11 13:40:53 +0100

    Improved FlexibleWrap

--- lib/src/flexible_wrap_base.dart
+++ lib/src/flexible_wrap_base.dart
@@ -5,7 +5,7 @@ import 'package:flutter/rendering.dart';
 /// A widget that arranges its children in a flexible wrap layout, allowing them
 /// to expand according to available space while maintaining the specified layout
 /// properties.
-class FlexibleWrap extends Wrap {
+class FlexibleWrap extends MultiChildRenderObjectWidget {
   /// Creates a FlexibleWrap widget.
   ///
   /// The [isOneRowExpanded] argument determines whether a single row of children
@@ -13,32 +13,43 @@ class FlexibleWrap extends Wrap {
   /// layout and alignment properties of the wrap.
   const FlexibleWrap(
       {super.key,
-      super.children,
-      super.textDirection,
-      super.spacing,
-      super.runSpacing,
+      required super.children,
+      this.textDirection,
+      this.spacing = 0.0,
+      this.runSpacing = 0.0,
       this.isOneRowExpanded = false});
 
   /// Whether a single row of children can expand to fill available space.
   final bool isOneRowExpanded;
+  final double spacing;
+  final double runSpacing;
+  final TextDirection? textDirection;
 
   @override
-  RenderWrap createRenderObject(BuildContext context) {
+  RenderFlexibleWrap createRenderObject(BuildContext context) {
     return RenderFlexibleWrap(
         spacing: spacing,
         textDirection: textDirection ?? Directionality.maybeOf(context),
         runSpacing: runSpacing,
-        isOneRowExpanded: isOneRowExpanded);
+        isOneRowExpanded: isOneRowExpanded)
+      ..markNeedsLayout();
   }
 
   @override
   void updateRenderObject(
       BuildContext context, RenderFlexibleWrap renderObject) {
-    renderObject
-      ..spacing = spacing
-      ..runSpacing = runSpacing
-      ..textDirection = textDirection ?? Directionality.maybeOf(context)
-      ..isOneRowExpanded = isOneRowExpanded;
+    if (renderObject.spacing != spacing ||
+        renderObject.runSpacing != runSpacing ||
+        renderObject.textDirection !=
+            (textDirection ?? Directionality.maybeOf(context)) ||
+        renderObject.isOneRowExpanded != isOneRowExpanded) {
+      renderObject
+        ..spacing = spacing
+        ..runSpacing = runSpacing
+        ..textDirection = textDirection ?? Directionality.maybeOf(context)
+        ..isOneRowExpanded = isOneRowExpanded;
+      renderObject.markNeedsLayout();
+    }
   }
 
   @override
